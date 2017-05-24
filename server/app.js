abf70bdd82847ebeb9f07ffe59f4f36c9c38a2dd
LucasMMBB <maoxummbb@gmail.com>
2017-05-24 17:37:10 -0400

    fixed server errors

--- server/app.js
+++ server/app.js
@@ -6,15 +6,14 @@ var index = require('./routes/index');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, '../onews-app/build/'));
+app.set('views', path.join(__dirname,'../onews-app/build/'));
 app.set('view engine', 'jade');
-
-app.use('/static', express.static(path.join(__dirname, '../client/build/static/')));
+app.use('/static', express.static(path.join(__dirname, '../onews-app/build/static/')));
 
 app.use('/', index);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function(req, res) {
   var err = new Error('Not Found');
   err.status = 404;
   res.render('404 Not Found');
