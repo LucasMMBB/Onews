5d033755d47711ae0c146183e08d4c609d709e63
LucasMMBB <maoxummbb@gmail.com>
2017-05-24 16:46:31 -0400

    setting up a server for this app and init it

---
+++ server/app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var path = require('path');
+
+var index = require('./routes/index');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, '../onews-app/build/'));
+app.set('view engine', 'jade');
+
+app.use('/static', express.static(path.join(__dirname, '../client/build/static/')));
+
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  res.render('404 Not Found');
+});
+
+
+module.exports = app;
