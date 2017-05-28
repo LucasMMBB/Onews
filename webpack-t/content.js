59668db72fb08cf4d31dc5c6ec668fe78b314be8
LucasMMBB <maoxummbb@gmail.com>
2017-05-27 21:23:36 -0400

    move test folder to another repository

--- webpack-t/content.js
+++
@@ -1,2 +0,0 @@
-export default  "It works from content.js.";
-// module.exports is es5 and export default is es6
--- webpack-t/entry.js
+++
@@ -1,7 +0,0 @@
-// ES5
-// require ("./style.css")
-// document.write(require('./content.js'));
-/*------- ES6 ------*/
-import './style.css';
-import content from './content.js';
-document.write(content);
