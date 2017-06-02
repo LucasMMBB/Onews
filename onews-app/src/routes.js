551fcc492f4bf7f0f85079277881f99cbe96da3b
LucasMMBB <maoxummbb@gmail.com>
2017-06-02 00:01:27 -0400

    init react-router file

---
+++ onews-app/src/routes.js
@@ -0,0 +1,6 @@
+import Base from './Base/Base';
+import App from './App/App';
+import LoginPage from './Login/LoginPage';
+import SignUpPage from './SignUp/SignUpPage';
+import Auth from './Auth/Auth';
+
