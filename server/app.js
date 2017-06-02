9e8af67603ee0a281ab52ecfce798dc18aaae85c
LucasMMBB <maoxummbb@gmail.com>
2017-06-02 17:28:06 -0400

    use cors

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+var cors = require('cors');
 var express = require('express');
 var path = require('path');
 
@@ -11,11 +12,7 @@ app.set('views', path.join(__dirname,'../onews-app/build/'));
 app.set('view engine', 'jade');
 app.use('/static', express.static(path.join(__dirname, '../onews-app/build/static/')));
 
-app.all("*", function(req, res, next){
-	res.header("Access-Control-Allow-Origin","*");
-	res.header("Access-Control-Allow-Headers","X-Requested-With");
-	next();
-});
+app.use(cors());
 
 app.use('/', index);
 app.use('/news', news);
