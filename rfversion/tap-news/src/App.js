aa639d7adfdceac1244e32d439401edd09e6d568
LucasMMBB <maoxummbb@gmail.com>
2017-08-21 15:59:46 -0400

    init refactor version of ieee

---
+++ rfversion/tap-news/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './assets/logo.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      	<img className='logo' src={logo} alt='tap-news'/>
+        <div className='container'>Hello Onews Project!</div>
+      </div>
+    );
+  }
+}
+
+export default App;
